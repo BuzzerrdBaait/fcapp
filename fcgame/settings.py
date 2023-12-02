295765ed98369d022c6bc8c6558fcbd4a0545ca1
Calvin G. <calcal56@gmail.com>
2023-12-02 13:18:51 -0600

    had sqlite set up

--- fcgame/settings.py
+++ fcgame/settings.py
@@ -126,22 +126,13 @@ DB_PASSWORD= os.environ.get('DB_PASSWORD')
 if IS_HEROKU_APP:
 
 
-    
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
     }
-
-
-#    DATABASES = {
-#        "default": dj_database_url.config(
-#            conn_max_age=600,
-#            conn_health_checks=True,
-#            ssl_require=True,
-#        ),
-#    }
 
 #######################################################
 else:
